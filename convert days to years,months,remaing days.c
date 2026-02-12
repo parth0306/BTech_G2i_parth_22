cf5002ec9883f59e1e1973054fe1ddc74a57dca8
parth0306 <25121cn146@glbitm.ac.in>
2026-02-12 12:17:08 +0530

    Create convert days to years,months,remaing days.c

---
+++ convert days to years,months,remaing days.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int d,m,y,r;
+    printf("Enter number of days =\t");
+    scanf("%d",&d);
+    y = d / 365;
+    m = d / 30;
+    r = d % 365;
+    printf("years=%d\n",y);
+    printf("months=%d\n",m);
+    printf("days=%d\n",r);
+    return 0;
+}
