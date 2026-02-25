3f855f10b89674819df0ba83aebe694069488c65
parth0306 <25121cn146@glbitm.ac.in>
2026-02-25 21:33:24 +0530

    q6.c
    write a program to check if the given two numbers are equal or not

---
+++ q6.c
@@ -0,0 +1,18 @@
+//write a program to check if the given two numbers are equal or not.
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter first number :\t");
+    scanf("%d",&a);
+    printf("\nEnter second number :\t ");
+    scanf("%d",&b);
+    if 
+        (a==b)
+        {
+            printf("both are equal");
+        }
+    else 
+        printf("not equal");
+    return 0;
+}
