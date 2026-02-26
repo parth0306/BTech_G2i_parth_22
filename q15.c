f984c5e30425ab6d6ad7388846c795dd9f4a76e2
parth0306 <25121cn146@glbitm.ac.in>
2026-02-26 12:12:03 +0530

    Fix Fibonacci series starting values
    Updated Fibonacci series initialization to start with 0 and 1.

--- q15.c
+++ q15.c
@@ -1,8 +1,7 @@
 //Write a program to print fibonacci series
 #include <stdio.h>
-#include <math.h>
 int main() {
-    int i, n, a = 1, b = 1, sum;
+    int i, n, a = 0, b = 1, sum;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Fibonacci Series: ");
