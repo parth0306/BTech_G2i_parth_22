f06a873b1d8ff6f05fee87faba2e648d7aeca1e8
parth0306 <25121cn146@glbitm.ac.in>
2026-03-19 11:31:36 +0530

    calculate power without pow()
    Implement a program to calculate power without using pow() function.

---
+++ find power without pow() function.c
@@ -0,0 +1,13 @@
+//Write a program to calculate power(x^y) without using pow() function.
+#include <stdio.h>
+int main() 
+{     int n,i,result=1,p;
+      printf("Enter a number : \n");
+      scanf("%d", &n); 
+      printf("enter power :\n");
+      scanf("%d", &p); 
+      for (i=1;i<=p;i++) 
+      {result*=n;}
+      printf("result=%d",result);
+       return 0;
+}
