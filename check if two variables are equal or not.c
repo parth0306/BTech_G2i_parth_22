3e63761db0d683e2285d61a0924870d67750cbe4
parth0306 <25121cn146@glbitm.ac.in>
2026-03-19 12:20:35 +0530

    check if the given two numbers are equal or not

--- check if two variables are equal or not.c
+++ check if two variables are equal or not.c
@@ -1,7 +1,8 @@
 //write a program to check if the given two numbers are equal or not.
 #include <stdio.h>
 int main()
-{int a,b;
+{
+    int a,b;
     printf("Enter first number :\t");
     scanf("%d",&a);
     printf("\nEnter second number :\t ");
