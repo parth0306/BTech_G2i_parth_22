21ed54eb6c96d9510434f7016f04a53a2efa7565
parth0306 <25121cn146@glbitm.ac.in>
2026-04-15 23:07:14 +0530

    Update 24_Find_min_and_max_element_of_an_array.c

--- 24_Find_min_and_max_element_of_an_array.c
+++ 24_Find_min_and_max_element_of_an_array.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 int main() 
 {
-    int n, i,arr[10],min = arr[10],max = '\0';
+    int n, i,arr[10],min,max;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     printf("Enter %d elements:\n", n);
     for(i = 0; i < n; i++) 
     {scanf("%d", &arr[i]);}
+    min=arr[0];
+    max=arr[0];
     for(i = 0; i < n; i++) 
     {   if(arr[i] < min) 
         {min = arr[i];}
-        if(arr[i] > max) 
+        else if(arr[i] > max) 
         {max = arr[i];}}
     printf("Minimum element = %d\n", min);
     printf("Maximum element = %d\n", max);
